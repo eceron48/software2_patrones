e349b5a64c04a1fa9c49247928c6ab4ef0fd6b09
Usuario <Usuario@DESKTOP-04R5SE0>
2018-09-12 15:13:53 -0500

    PATRON_SINGLETON FULL

---
+++ src/ingsw/pdd/singleton/ConfigurationSingleton.java
@@ -0,0 +1,82 @@
+package ingsw.pdd.singleton;
+
+import java.util.Properties;
+
+import ingsw.pdd.singleton.util.PropertiesUtil;
+
+public class ConfigurationSingleton {
+
+	private static ConfigurationSingleton singleton;
+	
+	private static final String CONFIGURATION_PROP="META-INF/configuration.properties";
+	private static final String APP_NAME_PROP="appName";
+	private static final String APP_VERSION_PROP="appVersion";
+
+	private String appName;
+	private String appVersion;
+	
+	private ConfigurationSingleton () {
+		
+		Properties prop =PropertiesUtil.loadProperty (CONFIGURATION_PROP);
+		this.appName=prop.getProperty(APP_NAME_PROP);
+		this.appVersion=prop.getProperty(APP_VERSION_PROP);
+		
+	}
+	
+	private static synchronized void createInstance() {
+		if(singleton==null) {
+			singleton=new ConfigurationSingleton();
+			
+		}
+		
+	}
+	public static ConfigurationSingleton getInstance() {
+		if (singleton==null) {
+			createInstance();
+		}
+		return singleton;
+	}
+
+	public static ConfigurationSingleton getSingleton() {
+		return singleton;
+	}
+
+	public static void setSingleton(ConfigurationSingleton singleton) {
+		ConfigurationSingleton.singleton = singleton;
+	}
+
+	public String getAppName() {
+		return appName;
+	}
+
+	public void setAppName(String appName) {
+		this.appName = appName;
+	}
+
+	public String getAppVersion() {
+		return appVersion;
+	}
+
+	public void setAppVersion(String appVersion) {
+		this.appVersion = appVersion;
+	}
+
+	public static String getConfigurationProp() {
+		return CONFIGURATION_PROP;
+	}
+
+	public static String getAppNameProp() {
+		return APP_NAME_PROP;
+	}
+
+	public static String getAppVersionProp() {
+		return APP_VERSION_PROP;
+	}
+
+	@Override
+	public String toString() {
+		return "ConfigurationSingleton [appName=" + appName + ", appVersion=" + appVersion + "]";
+	}
+	
+	
+}
---
+++ src/ingsw/pdd/singleton/util/PropertiesUtil.java
@@ -0,0 +1,24 @@
+package ingsw.pdd.singleton.util;
+
+import java.io.InputStream;
+import java.util.Properties;
+
+
+
+public class PropertiesUtil {
+	
+
+	public static Properties loadProperty(String propertiesURL) {
+		
+		try {
+			Properties properties=new Properties();
+			InputStream inputstream=PropertiesUtil.class.getClassLoader().getResourceAsStream(propertiesURL);
+			properties.load(inputstream);
+			return properties;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+}
